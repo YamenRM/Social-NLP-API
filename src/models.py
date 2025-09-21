249ae0b3f321e9ef990d461fb5c7c733113d6a9f
YamenRM <yamenrafat132@gmail.com>
2025-09-21 15:34:34 +0300

    updated

--- src/models.py
+++ src/models.py
@@ -2,7 +2,7 @@ from transformers import pipeline
 
 # Load the models from HF Hub
 
-sarcasm_model = pipeline("text-classification", model="YamenRM/sarcasm-detector")
+sarcasm_model = pipeline("text-classification", model="YamenRM/sarcasm_model")
 sentiment_model = pipeline("text-classification", model="YamenRM/Sentiment_model")
 toxicity_model = pipeline("text-classification", model="YamenRM/Toxicity_model")
 emotion_model = pipeline("text-classification", model="YamenRM/Emotion_model")
