1eb6fc8f2eb9eb745d9c3ddefccd17ab1a7bf503
YamenRM <yamenrafat132@gmail.com>
2025-09-21 14:36:26 +0300

    finshed the model loading

---
+++ src/models.py
@@ -0,0 +1,16 @@
+from transformers import pipeline
+
+# Load the models from HF Hub
+
+sarcasm_model = pipeline("text-classification", model="YamenRM/sarcasm-detector")
+sentiment_model = pipeline("text-classification", model="YamenRM/Sentiment_model")
+toxicity_model = pipeline("text-classification", model="YamenRM/Toxicity_model")
+emotion_model = pipeline("text-classification", model="YamenRM/Emotion_model")
+
+def get_models():
+    return {
+        "sarcasm": sarcasm_model,
+        "sentiment": sentiment_model,
+        "toxicity": toxicity_model,
+        "emotion": emotion_model
+    }
